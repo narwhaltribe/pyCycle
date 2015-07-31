7b4625f504af46868ff6d3f195ef2eeea971ff49
Brent Schroeter <brent@Brents-MacBook-Pro.local>
2015-07-31 01:46:36 -0700

    Rewrite compressor.py to comply with flowstation revisions

--- src/pycycle/compressor.py
+++ src/pycycle/compressor.py
@@ -1,85 +1,68 @@
 import math 
 
-from openmdao.main.api import Component
-from openmdao.lib.datatypes.api import Float, VarTree, Event
+from openmdao.core.component import Component
 
-from pycycle.flowstation import FlowStation, FlowStationVar, GAS_CONSTANT
+import pycycle.flowstation
 from pycycle.cycle_component import CycleComponent
 
-
-
 class Compressor(CycleComponent): 
-    """Axial Compressor performance calculations""" 
+    '''Axial Compressor performance calculations''' 
 
-    PR_des = Float(12.47, iotype="in", desc="Pressure ratio at design conditions")
-    MNexit_des = Float(.4, iotype="in", desc="mach number at the compressor exit at design conditions")
-    eff_des = Float(.95, iotype="in", desc="adiabatic efficiency at the design condition")
-    hub_to_tip = Float(.4, iotype="in", desc="ratio of hub radius to tip radius")
-    op_slope = Float(.85, iotype="in", desc="")
+    def __init__(self):
+        self.add_param('PR_des', 12.47, desc='Pressure ratio at design conditions')
+        self.add_param('MNexit_des', 0.4, desc='mach number at the compressor exit at design conditions')
+        self.add_param('eff_des', 0.95, desc='adiabatic efficiency at the design condition')
+        self.add_param('hub_to_tip', 0.4, desc='ratio of hub radius to tip radius')
+        self.add_param('op_slope', 0.85, desc='slope of operating line (pressure/efficiency)') # TODO add description
+        self.Fl_I_data = flowstation.init_flowstation(self.add_param, 'Fl_I') # incoming air stream to compressor
+        self.add_output('PR', 0.0, desc='pressure ratio at operating conditions')
+        self.add_output('eff', 0.0, desc='adiabatic efficiency at the operating condition')
+        self.add_output('eff_poly', 0.0, desc='polytropic efficiency at the operating condition')
+        self.add_output('pwr', 0.0, units='hp', desc='power required to run the compressor at the operating condition')
+        self.Fl_O_data = flowstation.init_flowstation(self.add_output, 'Fl_O') # outgoing air stream from compressor
+        self.add_output('tip_radius', 0.0, units='inch', desc='radius at the tip of the compressor')
+        self.add_output('hub_radius', 0.0, units='inch', desc='radius at the tip of the compressor')
 
-    Fl_I = FlowStationVar(iotype="in", desc="incoming air stream to compressor", copy=None)
-
-    PR = Float(iotype="out", desc="pressure ratio at operating conditions")
-    eff = Float(iotype="out", desc="adiabatic efficiency at the operating condition")
-    eff_poly = Float(iotype="out", desc="polytropic efficiency at the operating condition")
-    pwr = Float(iotype="out", units="hp", desc="power required to run the compressor at the operating condition")
-    Fl_O = FlowStationVar(iotype="out", desc="outgoing air stream from compressor", copy=None)
-    tip_radius = Float(iotype="out", units="inch", desc="radius at the tip of the compressor")
-    hub_radius = Float(iotype="out", units="inch", desc="radius at the tip of the compressor")
-
-
-    def _op_line(self,Wc): 
-        """relationship between compressor pressure ratio and mass flow""" 
-        b = 1 - self.op_slope #scaled PR and Wc at design are both 1
-        #assume a linear op line, with given slope
-
-        norm_PR = self.op_slope*(Wc/self._Wc_des) + b 
-
+    def _op_line(self, params, Wc): 
+        '''Relationship between compressor pressure ratio and mass flow''' 
+        b = 1 - params['op_slope'] # scaled PR and Wc at design are both 1
+        # assume a linear op line, with given slope
+        norm_PR = params['op_slope'] * (Wc / self._Wc_des) + b 
         return norm_PR*self.PR_des
 
-
-    def execute(self): 
-
-        Fl_I = self.Fl_I
-        Fl_O = self.Fl_O
-        fs_ideal = FlowStation()
-        Fl_O.W = Fl_I.W
-        
-        
+    def solve_nonlinear(self, params, unknowns, resids):
+        fs_ideal_data = flowstation.init_flowstation(self.add_output, 'fs_ideal')
+        unknowns['Fl_O:W'] = params['Fl_I:W']
         if self.run_design: 
-            #Design Calculations
-            Pt_out = self.Fl_I.Pt*self.PR_des
-            self.PR = self.PR_des
-            fs_ideal.setTotalSP(Fl_I.s, Pt_out)
-            ht_out = (fs_ideal.ht-Fl_I.ht)/self.eff_des + Fl_I.ht
-            Fl_O.setTotal_hP(ht_out, Pt_out)
-            Fl_O.Mach = self.MNexit_des
-            self._exit_area_des = Fl_O.area
-            self._Wc_des = Fl_I.Wc
-
+            # Design Calculations
+            Pt_out = params['Fl_I:Pt'] * params['PR_des']
+            unknowns['PR'] = params['PR_des']
+            flowstation.setTotalSP(unknowns, fs_ideal_data, params['Fl_I:s'], Pt_out)
+            ht_out = (fs_ideal.ht - params['Fl_I:ht']) / params['eff_des'] + params['Fl_I:ht']
+            flowstation.setTotal_hP(unknowns, Fl_O_data, ht_out, Pt_out)
+            unknowns['Fl_O:Mach'] = params['MNexit_des']
+            self._exit_area_des = unknowns['Fl_O:area']
+            self._Wc_des = params['Fl_I:Wc']
         else: 
-            #Assumed Op Line Calculation
-            self.PR = self._op_line(Fl_I.Wc)
-            self.eff = self.eff_des #TODO: add in eff variation with W
-            #Operational Conditions
-            Pt_out = Fl_I.Pt*self.PR
-            fs_ideal.setTotalSP(Fl_I.s, Pt_out)
-            ht_out = (fs_ideal.ht-Fl_I.ht)/self.eff + Fl_I.ht
-            Fl_O.setTotal_hP(ht_out, Pt_out)
-            Fl_O.area = self._exit_area_des #causes Mach to be calculated based on fixed area
+            # Assumed Op Line Calculation
+            unknowns['PR'] = self._op_line(params['Fl_I:Wc'])
+            unknowns['eff'] = parameters['eff_des'] # TODO: add in eff variation with W
+            # Operational Conditions
+            Pt_out = params['Fl_I:Pt'] * unknowns['PR']
+            flowstation.setTotalSP(unknowns, fs_ideal_data, params['Fl_I:s'], Pt_out)
+            ht_out = (unknowns['fs_ideal:ht'] - params['Fl_I:ht']) / unknowns['eff'] + params['Fl_I:ht']
+            flowstation.setTotal_hP(unknowns, Fl_O_data, ht_out, Pt_out)
+            unknowns['Fl_O:area'] = self._exit_area_des # causes Mach to be calculated based on fixed area
+        C = GAS_CONSTANT * math.log(unknowns['PR'])
+        delta_s = unknowns['Fl_O:s'] - params['Fl_I:s']
+        unknowns['eff_poly'] = C / (C + delta_s)
+        unknowns['pwr'] = params['Fl_I:W'] * (unknowns['Fl_O:ht'] - params['Fl_I:ht']) * 1.4148532 # btu/s to hp
+        unknowns['tip_radius'] = (unknowns['Fl_O:area'] / math.pi / (1 - params['hub_to_tip'] ** 2)) ** 0.5
+        unknowns['hub_radius'] = params['hub_to_tip'] * unknowns['tip_radius']
 
-        C = GAS_CONSTANT*math.log(self.PR)
-        delta_s = Fl_O.s - Fl_I.s
-        self.eff_poly = C/(C+delta_s)
-        self.pwr = Fl_I.W*(Fl_O.ht - Fl_I.ht) * 1.4148532 #btu/s to hp 
-        self.tip_radius = (Fl_O.area/math.pi/(1-self.hub_to_tip**2))**.5
-        self.hub_radius = self.hub_to_tip*self.tip_radius
-
-
-if __name__ == "__main__": 
-    from openmdao.main.api import set_as_top
-
-    c = set_as_top(Compressor())
-    c.run()
-
-
+if __name__ == '__main__': 
+    from openmdao.core.problem import Problem
+    
+    p = Problem(root=Compressor())
+    p.setup()
+    p.run()
