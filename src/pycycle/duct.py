070e95a5678e8d34c5fd3836562c0e0a9add376f
Brent Schroeter <brentsch@uw.edu>
2015-08-30 02:50:12 -0700

    Update duct.py

--- src/pycycle/duct.py
+++ src/pycycle/duct.py
@@ -1,43 +1,26 @@
-from openmdao.main.api import Component
-from openmdao.lib.datatypes.api import Float, VarTree
-
-from pycycle.flowstation import FlowStation, FlowStationVar
 from pycycle.cycle_component import CycleComponent
 
+class Duct(CycleComponent):
+    '''The inlet takes in air at a given flow rate and mach number, and diffuses it down to a slower mach number and larger area'''
+    def __init__(self):
+        super(CycleComponent, self).__init__()
+        self.add_param('dPqP', 0.0, desc='pressure differential as a fraction of incoming pressure')
+        self.add_param('Q_dot', 0.0, desc='heat flow rate into (positive) or out of (negative) the air', units='Btu/s')
+        self.add_param('MNexit_des', 0.6, desc='Mach number at the exit of the inlet')
+        self._add_flowstation('flow_in')
+        self._add_flowstation('flow_out')
 
-class Duct(CycleComponent): 
-    """The inlet takes in air at a given flow rate and mach number, and diffuses it down 
-    to a slower mach number and larger area"""
-
-    dPqP = Float(0.0, iotype="in", desc="pressure differential as a fraction of incomming pressure")
-    Q_dot = Float(0.0, iotype="in", units="Btu/s", desc="heat flow rate into (positive) or out of (negative) the air")
-    MNexit_des = Float(.6, iotype="in", desc="mach number at the exit of the inlet")
-
-    Fl_I = FlowStationVar(iotype="in", desc="incoming air stream to compressor", copy=None)
-    Fl_O = FlowStationVar(iotype="out", desc="outgoing air stream from compressor", copy=None)
-
-
-    def execute(self): 
-        Fl_I = self.Fl_I
-        Fl_O = self.Fl_O 
-
-        Pt_out = Fl_I.Pt*(1-self.dPqP)
-        q = self.Q_dot/Fl_I.W
-        Fl_O.setTotal_hP(Fl_I.ht+q, Pt_out)
-        Fl_O.W = Fl_I.W
-
-        if self.run_design: 
-            Fl_O.Mach = self.MNexit_des
-            self._exit_area_des = Fl_O.area
+    def solve_nonlinear(self, params, unknowns, resids):
+        self._solve_flow_vars('flow_in', params, unknowns)
+        Pt_out = unknowns['flow_in:out:Pt'] * (1.0 - params['dPqP'])
+        q = params['Q_dot'] / params['flow_in:in:W']
+        unknowns['flow_out:out:ht'] = unknowns['flow_in:out:ht'] + q
+        unknowns['flow_out:out:Pt'] = Pt_out
+        unknowns['flow_out:out:W'] = params['flow_in:in:W']
+        if params['design']: 
+            unknowns['flow_out:out:Mach'] = self.MNexit_des
+            self._solve_flow_vars('flow_out')
+            self._exit_area_des = unknowns['flow_out:out:area']
         else: 
-            Fl_O.area = self._exit_area_des
-
-
-
-
-if __name__ == "__main__": 
-    from openmdao.main.api import set_as_top
-
-    c = set_as_top(Inlet())
-    c.run()
-
+            unknowns['flow_out:out:area'] = self._exit_area_des
+            self._solve_flow_vars('flow_out')
