b92287df5edf66d38c105f8c68e06528445d93f5
Brent Schroeter <brentsch@uw.edu>
2015-08-31 02:02:22 -0700

    Solve with local Newton solver rather than higher level ScipyOptimizer driver

--- src/pycycle/heat_exchanger.py
+++ src/pycycle/heat_exchanger.py
@@ -15,6 +15,7 @@ NTU (effectiveness) Method
 
 import math
 
+from scipy.optimize import newton
 from pycycle.cycle_component import CycleComponent
 
 class HeatExchanger(CycleComponent):
@@ -27,33 +28,37 @@ class HeatExchanger(CycleComponent):
         self.add_param('effectiveness', 0.9765, desc='heat exchange effectiveness')
         self.add_param('MNexit_des', 0.6, desc='Mach number at exit')
         self.add_param('dPqP', 0.1, desc='pressure differential as a fraction of incoming pressure')
-        self.add_param('T_hot_out', 1400.0, desc='temperature of air out of heat exchanger', units='R')
-        self.add_param('T_cold_out', 518.0, desc='temperature of water out of heat exchanger' units='R')
         self._add_flowstation('flow_in')
 
+        self.add_output('T_hot_out', 1400.0, desc='temperature of air out of heat exchanger', units='R')
+        self.add_output('T_cold_out', 518.0, desc='temperature of water out of heat exchanger', units='R')
         self.add_output('Qreleased', 0.0, desc='energy released', units='hp')
         self.add_output('Qabsorbed', 0.0, desc='energy absorbed', units='hp')
         self.add_output('LMTD', 0.0, desc='logarithmic mean temperature difference')
         self.add_output('Qmax', 0.0, desc='theoretical maximum possible heat transfer', units='hp')
-        self.add_output('resid_Qmax', 0.0, desc='residual of max*effectiveness', units='hp')
-        self.add_output('resid_e_balance', 0.0, desc='residual of energy balance', units='hp')
         self._add_flowstation('flow_out')
 
     def solve_nonlinear(self, params, unknowns, resids):
+        self._clear_unknowns('flow_in', unknowns)
+        self._clear_unknowns('flow_out', unknowns)
         self._solve_flow_vars('flow_in', params, unknowns)
         W_cold_Cp_Min = min(params['W_cold'] * params['Cp_cold'], unknowns['flow_in:out:W'] * unknowns['flow_in:out:Cp'])
         unknowns['Qmax'] = W_cold_Cp_Min * (unknowns['flow_in:out:Tt'] - params['T_cold_in']) * 1.4148532 #BTU/s to hp
-        unknowns['Qreleased'] = unknowns['flow_in:out:W'] * unknowns['flow_in:out:Cp'] * (unknowns['flow_in:out:Tt'] - params['T_hot_out']) * 1.4148532
-        unknowns['Qabsorbed'] = params['W_cold'] * params['Cp_cold'] * (params['T_cold_out'] - params['T_cold_in']) * 1.4148532
+        T_out_guess = (unknowns['flow_in:out:Tt'] + params['T_cold_in']) / 2.0
+        def f(T_hot_out):
+            unknowns['Qreleased'] = unknowns['flow_in:out:W'] * unknowns['flow_in:out:Cp'] * (unknowns['flow_in:out:Tt'] - T_hot_out) * 1.4148532
+            return unknowns['Qreleased'] - params['effectiveness'] * unknowns['Qmax']
+        unknowns['T_hot_out'] = newton(f, T_out_guess)
+        def f(T_cold_out):
+            unknowns['Qabsorbed'] = params['W_cold'] * params['Cp_cold'] * (T_cold_out - params['T_cold_in']) * 1.4148532
+            return unknowns['Qreleased'] - unknowns['Qabsorbed']
+        unknowns['T_cold_out'] = newton(f, T_out_guess)
         try: 
-            unknowns['LMTD'] = (params['T_hot_out'] - unknowns['flow_in:out:Tt'] + params['T_cold_out'] - params['T_cold_in']) / math.log((params['T_hot_out'] - params['T_cold_in']) / (unknowns['flow_in:out:Tt'] - params['T_cold_out']))
+            unknowns['LMTD'] = (unknowns['T_hot_out'] - unknowns['flow_in:out:Tt'] + unknowns['T_cold_out'] - params['T_cold_in']) / math.log((unknowns['T_hot_out'] - params['T_cold_in']) / (unknowns['flow_in:out:Tt'] - unknowns['T_cold_out']))
         except ZeroDivisionError: 
             unknowns['LMTD'] = 0.0
-        unknowns['resid_Qmax'] = unknowns['Qreleased'] - params['effectiveness'] * unknowns['Qmax']
-        unknowns['resid_e_balance'] = unknowns['Qreleased'] - unknowns['Qabsorbed']
-
-        unknowns['flow_out:out:Tt'] = params['T_hot_out']
-        unknowns['flow_out:out:Pt'] = unknowns['flow_in:out:Pt'] * (1.0 - unknowns['dPqP'])
+        unknowns['flow_out:out:Tt'] = unknowns['T_hot_out']
+        unknowns['flow_out:out:Pt'] = unknowns['flow_in:out:Pt'] * (1.0 - params['dPqP'])
         unknowns['flow_out:out:W'] = unknowns['flow_in:out:W']
         if params['design']:
             unknowns['flow_out:out:Mach'] = params['MNexit_des']
